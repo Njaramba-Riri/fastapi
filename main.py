fc961d36e0814286bb95715000053260d445aa0f
Riri <jamesriri03@gmail.com>
2023-05-06 09:09:48 +0300

    update

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
-from fastapi.templating import jinja2
-from fastapi.
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 
 app=FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates=Jinja2Templates(directory="templates")
 
 class Item(BaseModel):
     name:str
@@ -16,11 +21,16 @@ class Item(BaseModel):
 async def read_root():
     return{"message":"Hello World"}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None]=None):
-    return{"item_id":item_id, "q":q}
+@app.get("/items/{item_id}", response_class=HTMLResponse)
+async def read_item(request: Request, id: str):
+    return templates.TemplateResponse("index.html", {"request": request, "id": id})
 
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item:Item):
     return{"item_name":item.name, "item_id":item_id}
+
+
+@app.get("/index")
+def index():
+    return Jinja2Templates('app/index.html')
